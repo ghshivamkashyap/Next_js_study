6240f183680306eb8b1ba4f5c1070fab64c93412
shivam digiprod <lootforecast@gmail.com>
2025-02-09 20:37:15 +0530

    intital comit first app started and tailwind used

---
+++ first-app-routing-app/app/about/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = () => {
+  return <div className=" text-sm   text-pink-500     ">i am about page</div>;
+};
+
+export default AboutPage;
---
+++ first-app-routing-app/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">hey shivam kashyap</main>
+    </div>
+  );
+}
