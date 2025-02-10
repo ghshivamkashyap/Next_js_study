82983bbb6ba54197e093872ab3bf8614a04b8373
shivam digiprod <lootforecast@gmail.com>
2025-02-10 21:54:54 +0530

    dynamic metadata on tab header name done

--- first-app-routing-app/app/page.js
+++ first-app-routing-app/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
-      <main className="">hey shivam kashyap</main>
+      <main className="">
+        i am home page
+        <Link className=" text-blue-500 text-xl ml-10" href={"/about"}>About</Link>
+      </main>
     </div>
   );
 }
